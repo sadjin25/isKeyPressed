078f57a7a822166190c97337cb3168230607634a
sadjin25 <qkrdnwls0101@naver.com>
2021-11-29 22:36:56 +0900

    add setConsoleView()

--- main.cpp
+++ main.cpp
@@ -1,20 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <windows.h>
 #include <iostream>
 #include <conio.h>
 
 //#define _CRT_NONSTDC_NO_WARNINGS    // 비표준 함수를 사용했을 때 에러 방지
 using namespace std;
 
+void setColsoleView() {
+    system("mode con:cols=50 lines=20");
+    system("title isKeyPressed");
+}
+
 int main() {
+    setColsoleView();
     char keyPress;
     int asciiValue;
-    cout<<"\n\t\tPress any key\n\n\t\t Press ESC to EXIT\n\n\n";
+    cout<<"\n\tPress any key\n\tPress ESC to EXIT\n\n\n";
     while(1) {
         keyPress = getch();
         asciiValue = keyPress;
 
-        if(asciiValue == 27) break;//ESC
-        cout << "\t\t\tKEY Pressed -> \" "<<keyPress<<" \" Ascii Value ->" <<asciiValue<<"\n\n";
+        if(asciiValue == 27) break;     //ESC
+        cout << "\tKEY Pressed -> \" "<<keyPress<<" \" Ascii Value ->" <<asciiValue<<"\n\n";
     }
+    return 0;
 }
